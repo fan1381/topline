1cefb332534d67d26058238976770f1aacdd730c
fan1381 <57989095+fan1381@users.noreply.github.com>
2020-01-07 09:37:20 +0800

    配置rem适配

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import './utils/register-vant' // 注册vant标签
+import 'amfe-flexible' // 动态设置rem基准值（html标签字体大小）
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/utils/register-vant.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import {
+  Button,
+  Cell
+} from 'vant'
+
+Vue.use(Button)
+  .use(Cell)
