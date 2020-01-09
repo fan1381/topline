75245d0d8ece8c0bd7cb38db8a98295fd3c2c61f
fan1381 <57989095+fan1381@users.noreply.github.com>
2020-01-09 10:11:50 +0800

    登录表单验证初始化配置

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import './utils/register-vant' // 注册vant标签
 import './styles/index.less'
+import './utils/validation'
 import 'amfe-flexible' // 动态设置rem基准值（html标签字体大小）
 Vue.config.productionTip = false
 
