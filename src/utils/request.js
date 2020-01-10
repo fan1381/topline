4a02b95073de63e9f7793ff4470135b66c4368b3
fan1381 <57989095+fan1381@users.noreply.github.com>
2020-01-10 17:06:33 +0800

    发送token

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import jsonBig from 'json-bigint' // 处理大数字
+import store from '@/store'
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/'
 })
@@ -11,4 +12,30 @@ request.defaults.transformResponse = [function (data) {
     return {}
   }
 }]
+// 请求 拦截器
+
+axios.interceptors.request.use(function (config) {
+  //  统一设置token
+  const { user } = store.state
+  if (user) {
+    // config.headers是用来获取本次请求头对象，这是axios固定的api
+    // Bearer后面有空格
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+  return config
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error)
+})
+
+// 响应拦截器
+axios.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  return response
+}, function (error) {
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  return Promise.reject(error)
+})
 export default request
